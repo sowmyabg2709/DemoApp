fce7b34f534fc19c24dedce495a10e71d0e868ba
sowmya.bg27 <sowmya.bg@sakshath-technologies.com>
2022-07-28 06:59:41 +0530

    added

--- src/test/java/com/qa/opencart/tests/AccountsPageTest.java
+++ src/test/java/com/qa/opencart/tests/AccountsPageTest.java
@@ -28,20 +28,20 @@ public class AccountsPageTest extends BaseTest{
 		Assert.assertEquals(title, Constants.ACCOUNTS_PAGE_TITLE);
 	}
 	
-	@Test
-	public void accountPageHeaderTest() {
-		String header=accPage.getAccountPageHeader();
-		System.out.println("Account page header is : " +header);
-		Assert.assertEquals(header, Constants.ACCOUNTS_PAGE_HEADER, Error.ACC_PAGE_TITLE_ERROR);
-	}
-	
-	@Test
-	public void accountSectionListTest() {
-		List<String> sectList=accPage.getAccountSection();
-		sectList.stream().forEach(e->System.out.println(e));
-		Collections.sort(Constants.EXP_ACC_SECLIST);
-		Assert.assertEquals(sectList, Constants.EXP_ACC_SECLIST);
-	}
+//	@Test
+//	public void accountPageHeaderTest() {
+//		String header=accPage.getAccountPageHeader();
+//		System.out.println("Account page header is : " +header);
+//		Assert.assertEquals(header, Constants.ACCOUNTS_PAGE_HEADER, Error.ACC_PAGE_TITLE_ERROR);
+//	}
+//	
+//	@Test
+//	public void accountSectionListTest() {
+//		List<String> sectList=accPage.getAccountSection();
+//		sectList.stream().forEach(e->System.out.println(e));
+//		Collections.sort(Constants.EXP_ACC_SECLIST);
+//		Assert.assertEquals(sectList, Constants.EXP_ACC_SECLIST);
+//	}
 	
 	@Test
 	public void logOutLinkTest() {		
